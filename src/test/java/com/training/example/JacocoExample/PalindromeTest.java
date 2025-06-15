81d670687d88563f00bd2894aeda6d55d56404ec
Dewanshu Singh <dewanshu90@gmail.com>
2025-06-15 21:55:43 +0530

    added more test cases

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 
 public class PalindromeTest {
 
-	// @Test
-	// public void whenEmptyString_thenAccept() {
-	//     Palindrome palindromeTester = new Palindrome();
-	//     assertTrue(palindromeTester.isPalindrome(""));
-	// }
+	@Test
+	public void whenEmptyString_thenAccept() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome(""));
+	}
 
 	@Test
 	public void whenSingleCharacter_thenAccept() {
@@ -17,27 +17,27 @@ public class PalindromeTest {
 	    assertTrue(palindromeTester.isPalindrome("a"));
 	}
 
-	// @Test
-	// public void whenPalindromeWord_thenAccept() {
-	//     Palindrome palindromeTester = new Palindrome();
-	//     assertTrue(palindromeTester.isPalindrome("madam"));
-	// }
+	@Test
+	public void whenPalindromeWord_thenAccept() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome("madam"));
+	}
 
-	// @Test
-	// public void whenNonPalindromeWord_thenReject() {
-	//     Palindrome palindromeTester = new Palindrome();
-	//     assertFalse(palindromeTester.isPalindrome("hello"));
-	// }
+	@Test
+	public void whenNonPalindromeWord_thenReject() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertFalse(palindromeTester.isPalindrome("hello"));
+	}
 
-	// @Test
-	// public void whenPalindromeWithEvenLength_thenAccept() {
-	//     Palindrome palindromeTester = new Palindrome();
-	//     assertTrue(palindromeTester.isPalindrome("noon"));
-	// }
+	@Test
+	public void whenPalindromeWithEvenLength_thenAccept() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome("noon"));
+	}
 
-	// @Test
-	// public void whenCaseSensitivePalindrome_thenReject() {
-	//     Palindrome palindromeTester = new Palindrome();
-	//     assertFalse(palindromeTester.isPalindrome("Madam"));
-	// }
+	@Test
+	public void whenCaseSensitivePalindrome_thenReject() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertFalse(palindromeTester.isPalindrome("Madam"));
+	}
 }
